900ae53929685f79e0dc09694d606bc5db578053
mash <mashnote@rambler.ru>
2022-06-16 17:24:55 +0300

    update test using assert

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,3 +1,4 @@
+import { strict as assert } from 'assert';
 import capitalize from '../src/capitalize.js';
 
 if (capitalize('hello') !== 'Hello') {
@@ -7,5 +8,11 @@ if (capitalize('hello') !== 'Hello') {
 if (capitalize('') !== '') {
   throw new Error('Функция работает неверно!');
 }
+
+assert(capitalize('') === '');
+assert(capitalize('hello') === 'Hello');
+
+assert.equal(capitalize(''), ' ');
+assert.equal(capitalize('hello'), 'Hello');
 
 console.log('Все тесты пройдены!');
